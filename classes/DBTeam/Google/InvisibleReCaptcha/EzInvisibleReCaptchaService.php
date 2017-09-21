8b4a511c18a9366f9539c33cbd0d0952733c0123
RadekZ <radoslaw.zadroga@db-team.pl>
2017-09-21 10:46:25 +0200

    Backend core functionality

--- classes/DBTeam/Google/InvisibleReCaptcha/EzInvisibleReCaptchaService.php
+++ classes/DBTeam/Google/InvisibleReCaptcha/EzInvisibleReCaptchaService.php
@@ -6,27 +6,42 @@ namespace DBTeam\Google\InvisibleReCaptcha;
 
 class EzInvisibleReCaptchaService
 {
+    const INI_FILENAME = "invisible_re_captcha.ini";
+    const INI_BLOCK_NAME = "Secret";
+    const INI_VAR_NAME__DEFAULT = "Default";
+    const INI_VAR_NANE__FOR_SITE_ACCESS = "ForSiteAccess";
+
+    /**
+     * @var string
+     */
     protected $secret = "";
+
 
     public function __construct()
     {
-    	$invisibleReCaptchaINI = \eZINI::instance('invisible_re_captcha.ini');
-        $secret = $invisibleReCaptchaINI->variable("Default", "Secret");
-        $secretForSiteAccess = $invisibleReCaptchaINI->variable("Default", "SecretForSiteAccess");
+    	$invisibleReCaptchaINI = \eZINI::instance(self::INI_FILENAME);
+    	/** @var string $secret */
+        $secret = $invisibleReCaptchaINI->variable(self::INI_BLOCK_NAME, self::INI_VAR_NAME__DEFAULT);
+        $secretForSiteAccess = $invisibleReCaptchaINI->variable(self::INI_BLOCK_NAME, self::INI_VAR_NANE__FOR_SITE_ACCESS);
         if(is_array($secretForSiteAccess) and $secretForSiteAccess)
         {
             /** @var array|null $sa */
             $sa = \eZSiteAccess::current();
-            /** @var string $saName */
-            $saName = $sa['name'];
-            if(isset($secretForSiteAccess[$saName]) and $secretForSiteAccess[$saName])
+            if($sa)
             {
-                $secret = $secretForSiteAccess[$saName];
+                /** @var string $saName */
+                $saName = $sa['name'];
+                if(isset($secretForSiteAccess[$saName]) and $secretForSiteAccess[$saName])
+                {
+                    $secret = $secretForSiteAccess[$saName];
+                }
             }
         }
         if(!$secret)
         {
-            throw new \Exception("Missing/empty INI param: " . '"Default", "Secret", "invisible_re_captcha.ini"');
+            throw new \Exception("Missing/empty INI param: "
+                . '"' . self::INI_BLOCK_NAME . '", "' . self::INI_VAR_NAME__DEFAULT . '", "' . self::INI_FILENAME . '"'
+            );
         }
 
         $this->secret = $secret;
